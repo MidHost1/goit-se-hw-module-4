d215347ba954b65bf4e6d3300583ad24b409a447
MidHost1 <maksim.frizorher123@gmail.com>
2026-03-01 19:30:23 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,16 +1,1 @@
-import threading
 
-def run_http():
-    print("HTTP сервер запущено.")
-
-def run_udp():
-    print("UDP сервер запущено.")
-    while True:
-        pass
-
-if __name__ == '__main__':
-    t1 = threading.Thread(target=run_http)
-    t2 = threading.Thread(target=run_udp)
-
-t1.start()
-t2.start()
